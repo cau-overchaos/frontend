1d7c1106a41524d33bd2fc122df47a92e84224f5
LiteHell <litehell@litehell.info>
2023-09-15 12:37:37 +0900

    Impl basic navbar

---
+++ src/app/main_navbar/main_navbar.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import Link from "next/link";
+import responsiveness from '../responsiveness.module.scss'
+import styles from './main_navbar.module.scss'
+import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBell, faSearch, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import UserPopup from "./user_popup";
+import NotificationPopup from "./nofitications_popup.module";
+
+type MenuItem = {
+    name: string;
+    href: string;
+}
+
+export default function MainNavbar() {
+    const [mobileMenuActive, setMobileMenuActive] = useState(false);
+    const [userPopupActive, setUserPopupActive] = useState(false);
+    const [notificationsActive, setNotificationsActive] = useState(false);
+    const menus: MenuItem[] = [
+        {
+            name: '내 스터디',
+            href: '#'
+        },
+        {
+            name: '스터디원 모집',
+            href: '#'
+        },
+        {
+            name: '코딩 테스트 대회',
+            href: '#'
+        }
+    ]
+
+    return <nav className={styles.navbar}>
+        <div className={classNames(responsiveness.container, styles.container)}>
+            <div className={styles.logo}>
+                <Link href="/">
+                    알고모여
+                </Link>
+            </div>
+            <div className={classNames(responsiveness.desktopOnly, styles.menu)}>
+                {menus.map(i => <div className={styles.item}><Link href={i.href}>
+                    {i.name}
+                </Link></div>)}
+            </div>
+            <div className={styles.icons}>
+                <div className={styles.item}>
+                    <Link href="#">
+                        <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                    </Link>
+                </div>
+                <div className={styles.item}>
+                    <Link href="#" onClick={() => {setUserPopupActive(false); setNotificationsActive(!notificationsActive)}}>
+                        <FontAwesomeIcon icon={faBell} ></FontAwesomeIcon>
+                    </Link>
+                    {notificationsActive && <NotificationPopup></NotificationPopup>}
+                </div>
+                <div className={styles.item}>
+                    <Link href="#" onClick={() => {setNotificationsActive(false); setUserPopupActive(!userPopupActive)}}>
+                        <FontAwesomeIcon icon={faUserCircle} ></FontAwesomeIcon>
+                    </Link>
+                    {userPopupActive && <UserPopup></UserPopup>}
+                </div>
+                <div className={styles.item}>
+                    <Link href="#" onClick={() => setMobileMenuActive(!mobileMenuActive)} className={responsiveness.mobileOnly}>
+                        <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        {mobileMenuActive && <div className={classNames(styles.mobileMenu, responsiveness.mobileOnly, responsiveness.container)}>
+            {menus.map(i => <div className={styles.item}><Link href={i.href} className={styles.item}>
+                {i.name}
+            </Link></div>)}
+        </div>}
+    </nav>;
+}
---
+++ src/app/main_navbar/user_popup.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames'
+import styles from './user_popup.module.scss'
+import popupStyles from './popup.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog, faSignIn, faSignOut } from '@fortawesome/free-solid-svg-icons'
+
+type propsType = {
+    nickname?: string;
+    backgroundUrl?: string;
+    loggedIn?: boolean;
+}
+
+export default function UserPopup(props: propsType) {
+    return <div className={popupStyles.container}>
+        <div className={popupStyles.popup}>
+            <div className={classNames(popupStyles.top, styles.profile)}>
+                <div className={styles.image} style={props.backgroundUrl ? { backgroundImage: `url(${props.backgroundUrl})` } : {}}></div>
+                <div className={styles.nickname}>{props.nickname ?? "나그네"}</div>
+            </div>
+            <hr className={popupStyles.divider}></hr>
+            <div className={popupStyles.bottom}>
+                <ul className={styles.menu}>
+                    <li>
+                        {props.loggedIn ?
+                            <a href="#">
+                                <FontAwesomeIcon className={styles.icon} icon={faSignOut}></FontAwesomeIcon>
+                                <span>로그아웃</span>
+                            </a> : <a href="#">
+                                <FontAwesomeIcon className={styles.icon} icon={faSignIn}></FontAwesomeIcon>
+                                <span>로그인</span>
+                            </a>
+                        }
+                    </li>
+                    {props.loggedIn &&
+                        <li>
+                            <a href="#">
+                                <FontAwesomeIcon className={styles.icon} icon={faCog}></FontAwesomeIcon>
+                                <span>설정</span>
+                            </a>
+                        </li>
+                    }
+                </ul>
+            </div>
+        </div>
+    </div>
+}
