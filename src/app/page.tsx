115861cb1151e8141ec044f9cb95d612d310eb53
LiteHell <litehell@litehell.info>
2023-09-15 14:33:57 +0900

    Impl board widget on index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import styles from './page.module.scss'
 import MainNavbar from "./main_navbar/main_navbar";
+import SummariedBoardWidget ,{ SummariedArticle } from "./summaried_board_widget/summaried_board_widget";
+import responsiveness from './responsiveness.module.scss'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.root}>
         <MainNavbar></MainNavbar>
+        <main className={responsiveness.container}>
+          <div className={styles.boards}>
+            <SummariedBoardWidget boardName="코딩테스트 / 대회 공고">
+            </SummariedBoardWidget>
+            <SummariedBoardWidget boardName="알고리즘 스터디 모임">
+            </SummariedBoardWidget>
+          </div>
+        </main>
     </div>
   )
 }
---
+++ src/app/summaried_board_widget/summaried_board_widget.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+import styles from './summaried_board_widget.module.scss'
+import Link from 'next/link';
+
+type BoardWidgetPropsType = {
+    boardName: string;
+    children?: ReactNode | ReactNode[]
+}
+
+type ArticlePropTypes = {
+    title: string;
+    href: string;
+    tags: string[];
+    startDate: Date;
+    dueDate: Date;
+}
+
+function formatDate(date: Date) {
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+}
+
+export default function SummariedBoardWidget(props: BoardWidgetPropsType) {
+    return <div className={styles.widget}>
+        <h2 className={styles.boardName}>{props.boardName}</h2>
+        <ul className={styles.articles}>
+            { props.children }
+        </ul>
+    </div>
+}
+
+export function SummariedArticle(props: ArticlePropTypes) {
+    return <li className={styles.article}>
+        <Link href={props.href}>
+            <div className={styles.title}>{props.title}</div>
+            <div className={styles.dueDate}>신청 마감일: {formatDate(props.startDate)}</div>
+            <div className={styles.startDate}>개최일: {formatDate(props.dueDate)}</div>
+            <ul className={styles.tags}>
+                {props.tags.map(i => <li className={styles.tag}>
+                    {i}
+                </li>)}
+            </ul>
+        </Link>
+    </li>
+}
