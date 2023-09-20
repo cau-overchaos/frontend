2e6971708c7ba671631bea901140d7c7bbc75aee
LiteHell <litehell@litehell.info>
2023-09-21 00:19:06 +0900

    Add responsiveness to leftmenu

--- src/app/study/left_menu/left_menu.tsx
+++ src/app/study/left_menu/left_menu.tsx
@@ -3,6 +3,8 @@ import styles from './left_menu.module.scss'
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import responsiveness from '../../responsiveness.module.scss'
+import classNames from 'classnames';
 
 type LeftMenuProps = {
     children: ReactNode;
@@ -26,7 +28,7 @@ export function LeftMenuItem(props: LeftMenuItemProps) {
     return <li>
         <Link href={props.href}>
             <div className={styles.icon}><FontAwesomeIcon icon={props.icon}></FontAwesomeIcon></div>
-            <div className={styles.text}>{props.text}</div>
+            <div className={classNames(styles.text, responsiveness.desktopOnly)}>{props.text}</div>
         </Link>
     </li>
 }
