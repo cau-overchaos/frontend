3f0c043bf88564c0544f4bb2f7acc120c89ac9af
LiteHell <litehell@litehell.info>
2023-09-25 21:19:32 +0900

    Impl api client

---
+++ src/app/api_client.ts
@@ -0,0 +1,59 @@
+type SignUpForm = {
+    userId: string;
+    password: string;
+    name: string;
+    judgeAccount: string;
+}
+
+type LoginForm = {
+    userId: string;
+    password: string;
+}
+
+type ApiResponse = {
+    status: string;
+    data: any;
+    message: string;
+}
+
+export default class ApiClient {
+    private apiEndpoint;
+    
+    /**
+     * Creates api client
+     * @param endpoint Root endpoint of api
+     */
+    constructor(endpoint: string = '/api') {
+        this.apiEndpoint = endpoint;
+    }
+
+    async login(form: LoginForm) {
+        const response = await fetch(this.apiEndpoint + '/login', {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (!response.ok) {
+            const responseData: ApiResponse = await response.json();
+            throw new Error(responseData.message);
+        };
+    }
+
+    async signUp(form: SignUpForm) {
+        const response = await fetch(this.apiEndpoint + '/signup', {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (!response.ok) {
+            const responseData: ApiResponse = await response.json();
+            throw new Error(responseData.message);
+        };
+    }
+}
