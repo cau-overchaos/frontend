81412217cb2160f7d2017d57cb0c398de02df412
LiteHell <litehell@litehell.info>
2023-09-26 23:00:14 +0900

    Impl event emitter for api client

--- src/app/api_client.ts
+++ src/app/api_client.ts
@@ -16,8 +16,13 @@ type ApiResponse = {
     message: string;
 }
 
+type EventType = 'loggedInOrloggedOut';
+
 class ApiClient {
     private apiEndpoint;
+    private listeners: {[key in EventType]: Function[]} = {
+        'loggedInOrloggedOut': []
+    }
     
     /**
      * Creates api client
@@ -27,10 +32,23 @@ class ApiClient {
         this.apiEndpoint = endpoint;
     }
 
+    on(type: EventType, listener: Function) {
+        this.listeners[type].push(listener);
+    }
+    
+    off(type: EventType, listener: Function) {
+        this.listeners[type] = this.listeners[type].filter(i => i !== listener);
+    }
+
+    private fireEvent (type: EventType) {
+        this.listeners[type].forEach(i => { setTimeout(i, 0); });
+    }
+
     async logout() {
         await fetch(this.apiEndpoint + '/logout', {
             method: 'POST'
         });
+        this.fireEvent('loggedInOrloggedOut');
     }
 
     async login(form: LoginForm) {
@@ -46,6 +64,8 @@ class ApiClient {
             const responseData: ApiResponse = await response.json();
             throw new Error(responseData.message);
         };
+
+        this.fireEvent('loggedInOrloggedOut');
     }
 
     async signUp(form: SignUpForm) {
