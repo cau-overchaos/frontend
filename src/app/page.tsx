6c42b14d2ee7444fd2299d393f1e38284835b901
LiteHell <litehell@litehell.info>
2023-10-07 01:18:50 +0900

    fetch participated, all studies on index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,61 @@
+"use client";
+
 import styles from "./page.module.scss";
-import MainNavbar from "./main_navbar/main_navbar";
 import SummariedBoardWidget, {
   SummariedArticle
 } from "./summaried_board_widget/summaried_board_widget";
-import responsiveness from "./responsiveness.module.scss";
-import Footer from "./footer/footer";
-import classNames from "classnames";
 import MainLayout from "./main_layout";
+import { useEffect, useState } from "react";
+import apiClient, { StudyRoom } from "./api_client";
 
 export default function Home() {
+  const [allLoaded, setAllLoaded] = useState<boolean>(false);
+  const [participatedLoaded, setParticipatedLoaded] = useState<boolean>(false);
+  const [all, setAll] = useState<StudyRoom[]>([]);
+  const [participated, setParticipated] = useState<StudyRoom[]>([]);
+
+  useEffect(() => {
+    if (!allLoaded) {
+      apiClient.studyrooms("all").then((all) => {
+        setAllLoaded(true);
+        setAll(all);
+      });
+    }
+
+    if (!participatedLoaded) {
+      apiClient
+        .studyrooms("participated")
+        .then((participated) => {
+          setParticipatedLoaded(true);
+          setParticipated(participated);
+        })
+        .finally(() => {
+          setParticipatedLoaded(true);
+        });
+    }
+  });
+
   return (
     <MainLayout>
       <div className={styles.boards}>
-        <SummariedBoardWidget boardName="코딩테스트 / 대회 공고"></SummariedBoardWidget>
-        <SummariedBoardWidget boardName="알고리즘 스터디 모임"></SummariedBoardWidget>
+        <SummariedBoardWidget boardName="알고리즘 스터디 모임">
+          {all.map((i) => (
+            <SummariedArticle
+              title={i.title}
+              href={`/study/${i.id}`}
+              tags={[]}
+            ></SummariedArticle>
+          ))}
+        </SummariedBoardWidget>
+        <SummariedBoardWidget boardName="내 스터디">
+          {participated.map((i) => (
+            <SummariedArticle
+              title={i.title}
+              href={`/study/${i.id}`}
+              tags={[]}
+            ></SummariedArticle>
+          ))}
+        </SummariedBoardWidget>
       </div>
     </MainLayout>
   );
