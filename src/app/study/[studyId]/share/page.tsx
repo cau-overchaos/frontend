11acfece28361f6e6dd7f2068754fb46d37e269b
LiteHell <litehell@litehell.info>
2023-11-10 15:03:58 +0900

    Fetch shared codes on list page

--- src/app/study/[studyId]/share/page.tsx
+++ src/app/study/[studyId]/share/page.tsx
@@ -1,22 +1,49 @@
+"use client";
+
 import Board, { Article } from "@/app/board/board";
 import SolvedAcTier from "../assignments/solved_ac_tier";
 import styles from "./page.module.scss";
+import { useEffect, useState } from "react";
+import apiClient, { SharedSourceCode } from "@/app/api_client/api_client";
+import { useParams } from "next/navigation";
 
 export default function SharePage() {
+  const params = useParams();
+  const [sharedCodes, setSharedCodes] = useState<SharedSourceCode[]>([]);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (sharedCodes.length === 0)
+      apiClient
+        .studyroom(parseInt(params.studyId as string))
+        .sharedSourceCodes()
+        .then(setSharedCodes)
+        .catch((err) => setError(err.message))
+        .then(() => setError(undefined));
+  }, [sharedCodes]);
+
   return (
     <div className={styles.padded}>
-      <Board title="코드 공유" withProblem>
-        <Article
-          title="Lorem ipsum"
-          author="asdf"
-          date={new Date()}
-          href="share/1"
-          problem={{
-            id: "15649",
-            tier: <SolvedAcTier level={8}></SolvedAcTier>,
-            title: "N과 M (1)"
-          }}
-        ></Article>
+      <Board
+        title="코드 공유"
+        withProblem
+        loading={sharedCodes.length === 0}
+        error={error}
+      >
+        {sharedCodes.map((i) => (
+          <Article
+            title={i.title}
+            author={i.writer.nickname}
+            date={new Date()}
+            href={`share/${i.id}`}
+            problem={{
+              tier: (
+                <SolvedAcTier level={i.problem.difficultyLevel}></SolvedAcTier>
+              ),
+              title: i.problem.title
+            }}
+          ></Article>
+        ))}
       </Board>
     </div>
   );
