ff1eda6aecc8168c409d0e2cf7ba2a7a787349fe
LiteHell <litehell@litehell.info>
2023-11-14 17:17:57 +0900

    Impl comments on recruit post

--- src/app/study/wanted/[articleId]/comments.tsx
+++ src/app/study/wanted/[articleId]/comments.tsx
@@ -1,5 +1,9 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useState } from "react";
 import styles from "./comments.module.scss";
+import { Button, Input } from "@/app/common/inputs";
+import DefaultProfileImageUrl from "@/app/default_profile_image_url";
 
 type CommentProp = {
   inviteButton: boolean;
@@ -11,15 +15,66 @@ type CommentProp = {
 
 type CommentsProp = {
   children: ReactNode;
+  profileImgUrl?: string;
   onNewComment: (comment: string) => void;
 };
 
-export function Comment() {}
+export function Comment(props: CommentProp) {
+  return (
+    <div className={styles.comment}>
+      <div
+        className={styles.profileImg}
+        style={{
+          backgroundImage: `url("${
+            props.profileImgUrl ?? DefaultProfileImageUrl()
+          }")`
+        }}
+      ></div>
+      <div className={styles.content}>
+        {props.comment}{" "}
+        <span className={styles.author}>--{props.nickname}</span>
+      </div>
+      {props.inviteButton && (
+        <div className={styles.inviteBtn}>
+          <Button small onClick={props.onInviteClick}>
+            초대
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default function Comments(props: CommentsProp) {
+  const [newComment, setNewComment] = useState<string>("");
+
   return (
     <div className={styles.comments}>
-      <form className={styles.newComment}></form>
+      <hr></hr>
+      <form
+        className={styles.newComment}
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          props.onNewComment(newComment);
+        }}
+      >
+        <div
+          className={styles.profileImg}
+          style={{
+            backgroundImage: `url("${
+              props.profileImgUrl ?? DefaultProfileImageUrl()
+            }")`
+          }}
+        ></div>
+        <div className={styles.input}>
+          <Input
+            placeholder="새 댓글을 입력해주세요..."
+            value={newComment}
+            onChange={(evt) => setNewComment(evt.target.value)}
+          ></Input>
+        </div>
+      </form>
+      {props.children}
     </div>
   );
 }
--- src/app/study/wanted/[articleId]/page.tsx
+++ src/app/study/wanted/[articleId]/page.tsx
@@ -1,24 +1,65 @@
 "use client";
 
 import apiClient from "@/app/api_client/api_client";
-import { RecruitPost } from "@/app/api_client/recruits";
+import { RecruitPost, RecruitPostComment } from "@/app/api_client/recruits";
 import Article from "@/app/board/article";
 import MainLayout from "@/app/main_layout";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Comments, { Comment } from "./comments";
 
 export default function WantedArticlePage() {
   const params = useParams();
   const [post, setPost] = useState<RecruitPost | null>(null);
+  const [comments, setComments] = useState<RecruitPostComment[] | null>(null);
+  const [refreshComments, setRefreshComments] = useState<boolean>(false);
+  const [isCurrentUserStudyroomManager, setIsCurrentUserStudyroomManager] =
+    useState<boolean>(false);
+  const articleId = parseInt(params.articleId as string);
 
   useEffect(() => {
     if (post === null)
       apiClient
         .recruitPosts()
-        .getPost(parseInt(params.articleId as string))
+        .getPost(articleId)
         .then(setPost)
         .catch((i) => alert(`오류: ${i.message}`));
-  });
+  }, [post]);
+
+  useEffect(() => {
+    if (comments === null || refreshComments)
+      apiClient
+        .recruitPosts()
+        .getComments(articleId)
+        .then((i) => {
+          if (refreshComments) setRefreshComments(false);
+          setComments(i.comments);
+          setIsCurrentUserStudyroomManager(i.isCurrentUserStudyRoomManager);
+        });
+  }, [comments, refreshComments]);
+
+  const postNewComment = (comment: string) => {
+    apiClient
+      .recruitPosts()
+      .createComment(articleId, comment)
+      .then(() => {
+        setRefreshComments(true);
+      });
+  };
+
+  const inviteUser = (userId: string) => {
+    if (post === null) return alert("잠시후 다시 시도해주세요.");
+    apiClient
+      .studyroom(post?.studyRoom.id)
+      .addMember(userId)
+      .then(() => {
+        alert("초대했습니다!");
+        setRefreshComments(true);
+      })
+      .catch((err) => {
+        alert(`오류가 발생했습니다: ${err.message}`);
+      });
+  };
 
   return post === null ? (
     <div></div>
@@ -34,6 +75,20 @@ export default function WantedArticlePage() {
       >
         {post.content}
       </Article>
+      <Comments onNewComment={postNewComment}>
+        {comments?.map((i) => (
+          <Comment
+            inviteButton={
+              isCurrentUserStudyroomManager && !i.writer.alreadyStudyMember
+            }
+            nickname={i.writer.name}
+            comment={i.comment}
+            onInviteClick={() => {
+              inviteUser(i.writer.id);
+            }}
+          ></Comment>
+        ))}
+      </Comments>
     </MainLayout>
   );
 }
